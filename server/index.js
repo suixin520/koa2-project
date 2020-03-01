fe7e52c967a26a09aa0d4e3df8a6890046428302
songbo <songbo_work@163.com>
2020-03-01 10:19:02 +0800

    :sparkles: add koa-views

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
 
-const { htmlTpl, ejsTpl, pugTpl} = require('./tpl')
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-  ctx.body = pug.render(pugTpl, {
+  await ctx.render('index', {
     you: '你好',
     me: '随心'
   })
