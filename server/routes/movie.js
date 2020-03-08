661456bf97bdd394b79a8e04de5819915457d2ff
songbo <songbo_work@163.com>
2020-03-08 11:33:38 +0800

    :sparkles: home page & detail page

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -10,15 +10,16 @@ const {
   getRelativeMovies
 } = require('../service/movie')
 
-
 @controller('/api/v0/movies')
 export class movieController {
   @get('/')
   async getMovies (ctx, next) {
     const { type, year } = ctx.query
     const movies = await getAllMovies(type, year)
-    ctx.body ={
-      movies
+
+    ctx.body = {
+      success: true,
+      data: movies
     }
   }
 
@@ -27,6 +28,7 @@ export class movieController {
     const id = ctx.params.id
     const movie = await getMovieDetail(id)
     const relativeMovies = await getRelativeMovies(movie)
+
     ctx.body = {
       data: {
         movie,
